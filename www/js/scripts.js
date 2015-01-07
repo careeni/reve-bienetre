6ef7ca4a2073fde5fc425aeb26faea655a226603
careeni <carine.du@gmail.com>
2015-01-07 23:17:25 +0100

    Restructuration catégorie + Nouvelle charte graphique
    Ajout de sous-catégorie + nouvelle charte  graphique

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -18,7 +18,42 @@
 		$(this).next(".toggle-content").slideToggle();
 		//$(this).next(".toggle-content").slideDown();
 	});
+	
+	init_nbProduits();	
+	
+	
+	/*$(".produits-sous-nav a").click(function(){
+		$(".produits").hide();
+		var el = $(this).attr('class');
+		$('.liste-produits'+' .'+el).hide().fadeIn();
+		return false;
+	});
+	
+	
+	$(".produits-gommages-raffermissant").click(function(){
+		var nb_item= $("li.produits-gommages-raffermissant").length;	
+		$("#nbItem_corps").text(nb_item);
+	});
+	$(".produits-huiles").click(function(){
+		var nb_item= $("li.produits-huiles").length;	
+		$("#nbItem_corps").text(nb_item);
+	});
+	$(".produits-baumes-beurres").click(function(){
+		var nb_item= $("li.produits-baumes-beurres").length;	
+		$("#nbItem_corps").text(nb_item);
+	});
+	$(".produits-corps").click(function(){
+		init_nbProduits();
+	});*/
+	
+	
 });
+
+function init_nbProduits() {
+	var nb_item= $(".produits").length;	
+	$("#nbItem_corps").text(nb_item);
+	/*$('.liste-produits .produits').hide().fadeIn();*/
+}
  
  
 
